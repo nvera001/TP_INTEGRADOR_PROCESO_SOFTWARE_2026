984eb2c1cda53514d99379dac6ea6dcb96c70cc6
LGhiglionee <lghiglione@uade.edu.ar>
2026-06-30 21:08:47 -0300

    Sonido

--- TP_La_Mango/src/vista/GestorAudio.java
+++ TP_La_Mango/src/vista/GestorAudio.java
@@ -6,6 +6,10 @@ import java.net.URL;
 public class GestorAudio {
     private static boolean sonidoActivo = true;
 
+    private long tiempoUltimoSonido = 0;
+
+    private final int TIEMPO_ESPERA = 300;
+
     public static void toggleSonido() {
         sonidoActivo = !sonidoActivo;
     }
@@ -17,19 +21,26 @@ public class GestorAudio {
     public void reproducirSonido(String nombreArchivo) {
         if (!sonidoActivo) return;
 
-        try {
-            URL urlStream = getClass().getResource("/vista/recursos/audios/" + nombreArchivo);
+        long tiempoActual = System.currentTimeMillis();
 
-            if (urlStream != null) {
-                AudioInputStream audioStream = AudioSystem.getAudioInputStream(urlStream);
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioStream);
-                clip.start();
-            } else {
-                System.out.println("No se encontró el archivo de audio en: /Vista/recursos/audios/" + nombreArchivo);
+        if (tiempoActual - tiempoUltimoSonido >= TIEMPO_ESPERA) {
+
+            tiempoUltimoSonido = tiempoActual;
+
+            try {
+                URL urlStream = getClass().getResource("/vista/recursos/audios/" + nombreArchivo);
+
+                if (urlStream != null) {
+                    AudioInputStream audioStream = AudioSystem.getAudioInputStream(urlStream);
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioStream);
+                    clip.start();
+                } else {
+                    System.out.println("No se encontró el archivo de audio en: /vista/recursos/audios/" + nombreArchivo);
+                }
+            } catch (Exception e) {
+                System.out.println("Error al reproducir sonido: " + e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println("Error al reproducir sonido: " + e.getMessage());
         }
     }
 }
